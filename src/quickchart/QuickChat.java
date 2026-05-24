c6e4521e2652801c0b8f1fb121be33d9768ff9e7
ntshembhof-bit <ntshembhof@gmail.com>
2026-05-24 23:16:53 +0200

    added send , sttore and message counter functionality

--- src/quickchart/QuickChat.java
+++ src/quickchart/QuickChat.java
@@ -1,3 +1,5 @@
+package quickchart;
+
 import java.util.Scanner;
 import java.util.Random;
 
@@ -7,6 +9,8 @@ public class QuickChat {
 
         Scanner input = new Scanner(System.in);
         Random random = new Random();
+
+        int totalMessages = 0;
 
         System.out.println("Welcome to QuickChat.");
         System.out.println("-----------------------");
@@ -26,7 +30,7 @@ public class QuickChat {
 
                 Message msg = new Message(1);
 
-                // Generate Message ID
+                
                 long messageID =
                         1000000000L +
                         (long)(random.nextDouble()
@@ -43,31 +47,70 @@ public class QuickChat {
 
                 String cell = input.nextLine();
 
-                System.out.println(
-                        msg.checkRecipientCell(cell));
+                String cellCheck =
+                        msg.checkRecipientCell(cell);
 
-                // Message
+                System.out.println(cellCheck);
+
+                if (!cellCheck.contains("successfully")) {
+
+                    continue;
+                }
+
                 System.out.print(
                         "Enter Message: ");
 
                 String text = input.nextLine();
 
-                System.out.println(msg.checkMessageLength(text));
+                String lengthCheck =
+                        msg.checkMessageLength(text);
 
-                // Create Hash
-                String hash = msg.createMessageHash(text);
+                System.out.println(lengthCheck);
 
-                System.out.println("Message Hash: " + hash);
+                if (text.length() > 250) {
 
-                // Send Option
+                    continue;
+                }
+
+                String hash =
+                        msg.createMessageHash(text);
+
                 System.out.println();
+                System.out.println(
+                        "What would you like to do?");
+
                 System.out.println("1) Send Message");
                 System.out.println("2) Disregard Message");
+                System.out.println("3) Store Message");
 
-                int choice = input.nextInt();
+                System.out.print("Choice: ");
+
+                int action = input.nextInt();
                 input.nextLine();
 
-                msg.sentMessage(choice));
+                System.out.println(
+                        msg.sentMessage(action));
+
+                if (action == 1) {
+
+                    totalMessages++;
+
+                    System.out.println();
+                    System.out.println(
+                            "--- Sent Message Details ---");
+
+                    System.out.println(
+                            "Message ID: " + id);
+
+                    System.out.println(
+                            "Message Hash: " + hash);
+
+                    System.out.println(
+                            "Recipient: " + cell);
+
+                    System.out.println(
+                            "Message: " + text);
+                }
 
             } else if (menuChoice.equals("2")) {
 
@@ -82,6 +125,9 @@ public class QuickChat {
                 System.out.println("Invalid choice.");
             }
         }
+
+        System.out.println();
+        System.out.println("Total number of messages sent: " + totalMessages);
 
         System.out.println("Thank you for using QuickChat!");
     }
