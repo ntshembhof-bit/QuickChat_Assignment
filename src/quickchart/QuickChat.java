8fb3f7714b0f00de3d905b05c98d7d7831250fac
ntshembhof-bit <ntshembhof@gmail.com>
2026-05-24 21:56:09 +0200

    added a scanner , menu to choose the option you want

--- src/quickchart/QuickChat.java
+++ src/quickchart/QuickChat.java
@@ -1,8 +1,44 @@
-package quickchart;{
+import java.util.Scanner;
 
-  public static void main(String[] args);{
-  
-     System.out.println("Welcome to QuickChat.");
-}    
-}        
+public class QuickChat {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to QuickChat.");
+        System.out.println("-----------------------");
+
+        while (true) {
+
+            System.out.println();
+            System.out.println("MENU:");
+            System.out.println("1) Send Messages");
+            System.out.println("2) Show recently sent messages");
+            System.out.println("3) Quit");
+            System.out.print("Select an option: ");
+
+            String menuChoice = input.nextLine();
+
+            if (menuChoice.equals("1")) {
+
+                System.out.println("Send Messages selected.");
+
+            } else if (menuChoice.equals("2")) {
+
+                System.out.println("Coming Soon.");
+
+            } else if (menuChoice.equals("3")) {
+
+                break;
+
+            } else {
+
+                System.out.println("Invalid choice.");
+            }
+        }
+
+        System.out.println("Thank you for using QuickChat!");
+    }
+}   
 
